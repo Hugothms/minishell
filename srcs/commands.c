58e3542e4e41bb5ebba9ba659a2ee7aea967f4cd
Hugothms <hugothms@gmail.com>
2020-09-20 18:06:56 +0200

    exit & echo

--- srcs/commands.c
+++ srcs/commands.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 15:52:58 by hthomas           #+#    #+#             */
-/*   Updated: 2020/09/18 16:17:00 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/09/20 18:06:31 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,52 @@
 
 char	*ft_pwd(void)
 {
-	return(getcwd(NULL, 0));
+	char	*ret;
+	char	*tmp;
+
+	ret = getcwd(NULL, 0);
+	tmp = ret;
+	ret = ft_strjoin(ret, "\n");
+	free(tmp);
+	return(ret);
 }
 
 char	*ft_echo(char **args)
 {
 	int		i;
+	int		nflag;
 	char	*ret;
+	char	*tmp;
 
+	nflag = 0;
+	ret = malloc(sizeof(char));
+	*ret = '\0';
 	if (!*args)
-	{
-		ret = malloc(sizeof(char));
-		*ret = '\0';
 		return (ret);
-	}
 	i = 0;
-	if (ft_strcmp(args[i], "-n"))
-		i++;
-	// if (echo_args_valid())
+	while (!ft_strcmp(args[i], "-n"))
 	{
-		while(args[i])
+		nflag = 1;
+		i++;
+	}
+	while(args[i])
+	{
+		tmp = ret;
+		ret = ft_strjoin(ret, args[i]);
+		free(tmp);
+		if (args[i + 1])
 		{
-			ft_strjoin(ret, args[i]);
-			if (args[i + 1])
-				ft_strjoin(args[i], " ");
-			i++;
-		}
+			tmp = ret;
+			ret = ft_strjoin(ret, " ");
+			free(tmp);
+		}		
+		i++;
+	}
+	if(!nflag)
+	{
+		tmp = ret;
+		ret = ft_strjoin(ret, "\n");
+		free(tmp);
 	}
 	return (ret);
 }
@@ -56,6 +76,9 @@ char	*ft_env(char **args)
 
 char	*ft_exit(char **args)
 {
+	if (*args)
+		exit(**args - '0');
+	exit(0);
 	return(NULL);
 }
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 19:21:43 by hthomas           #+#    #+#             */
-/*   Updated: 2020/09/18 16:08:09 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/09/20 18:05:41 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 char	*exec_command(char **command)
 {
+	char *ret;
+	char *tmp;
+
 	if(!command)
 		return (NULL);
-	else if (!(command[0]))
-		return (NULL);
+	// else if (!(command[0]))
+	// 	return (NULL);
 	else if (!ft_strcmp(command[0], "echo"))
 		return(ft_echo(&command[1]));
 	else if (!ft_strcmp(command[0], "cd"))
@@ -33,15 +36,21 @@ char	*exec_command(char **command)
 	else if (!ft_strcmp(command[0], "exit"))
 		return(ft_exit(&command[1]));
 	else
-		return (ft_strjoin("minishell: command not found: ", command[0]));
+	{
+		ret = ft_strjoin("minishell: command not found: ", command[0]);
+		tmp = ret;
+		ret = ft_strjoin(ret, "\n");
+		free(tmp);
+		return (ret);
+	}
 }
 
-void	print_pwd_msg()
+void	print_console_msg()
 {
 	char	*pwd;
 
 	ft_putstr("\xE2\x9E\xA1 ");
-	pwd = ft_pwd();
+	pwd = getcwd(NULL, 0);
 	ft_putstr(pwd);
 	free(pwd);
 	ft_putstr(": ");
@@ -58,23 +67,24 @@ int		main(const int argc, const char *argv[])
 	while(1)
 	{
 		free(input);
-		print_pwd_msg();
+		print_console_msg();
 		get_next_line_custom(&input);
 		if(parse_input(input, &command))
 		{
 			ft_putstr("minishell: parse error\n");
+			free(input);
 			exit(1);
 		}
-		if(command)
+		if(*command)
 		{
 			if(ret = exec_command(command))
 			{
-				ft_putstr_clean(ret);
+				ft_putstr(ret);
 				free(ret);
 			}
-			ft_free_tab(command);
-			free(command);
+			ft_free_tab(command);	
 		}
+		free(command);
 	}
 	free(input);
 	return (0);
