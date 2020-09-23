9245e59b99215d0a3dbaac65b215ae54593df120
Hugothms <hugothms@gmail.com>
2020-09-23 19:00:39 +0200

    update libft

--- libft/string/ft_strjoin_free.c
+++ libft/string/ft_strjoin_free.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_len_hex.c                                 :+:      :+:    :+:   */
+/*   ft_strjoin_free.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/01 16:53:02 by hthomas           #+#    #+#             */
-/*   Updated: 2019/12/05 16:43:54 by hthomas          ###   ########.fr       */
+/*   Created: 2020/09/23 18:06:43 by hthomas           #+#    #+#             */
+/*   Updated: 2020/09/23 18:06:46 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			ft_count_len_hex(unsigned long long hex)
-{
-	int	res;
+#include "libft.h"
 
-	res = 0;
-	if (hex == 0)
-		return (1);
-	while (hex)
-	{
-		res++;
-		hex /= 16;
-	}
-	return (res);
+char	*ft_strjoin_free(char *s1, const char *s2)
+{
+	char	*tmp;
+
+	tmp = s1;
+	s1 = ft_strjoin(s1, s2);
+	free(tmp);
+	return (s1);
 }
