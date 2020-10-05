b53258b538527af84d75ac73594dd8cf52654194
Hugothms <hugothms@gmail.com>
2020-10-06 00:14:01 +0200

    clean

--- srcs/search_command.c
+++ srcs/search_command.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 13:04:47 by hthomas           #+#    #+#             */
-/*   Updated: 2020/10/05 11:32:35 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/10/06 00:13:50 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,8 +80,6 @@ int		search_command(t_list_command *command, char **envp)
 
 	ret = 1;
 	pid_t pid = fork();
-	ft_putstr("azerty\n");
-	ft_putnbr(pid);
 	if (pid == 0)
 	{
 		if (try_path(command, envp))
@@ -92,6 +90,5 @@ int		search_command(t_list_command *command, char **envp)
 		ret = 0;
 		wait(&status);
 	}
-	ft_putnbr(pid);
 	return (ret);
 }
