5ae1af7aa86fe07076651c29cd046a4a9cb32b9b
Hugothms <hugothms@gmail.com>
2020-10-06 17:34:40 +0200

    coments

--- srcs/parse_quotes.c
+++ srcs/parse_quotes.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse2.c                                           :+:      :+:    :+:   */
+/*   parse_quotes.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 01:10:40 by hthomas           #+#    #+#             */
-/*   Updated: 2020/10/04 13:27:33 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/10/06 13:00:08 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,16 @@ void	add_substr_to_cmd(char *input, t_list_command **command, int start, int siz
 
 void	simple_quotes(char *input, t_list_command **command, t_parse *par)
 {
-	if(!par->in_simple && par->i && !ft_in_charset(input[par->i - 1], WHITESPACES))		
+	if(!par->in_simple && par->i && !ft_in_charset(input[par->i - 1], WHITESPACES))
+		// start quotes just after a word
 		add_substr_to_cmd(input, command, par->pos, par->i - par->pos - 1, NO_SPACE_AFTER);
 	else if(par->in_simple)
 	{
 		if (input[par->i + 1] && !ft_in_charset(input[par->i + 1], WHITESPACES))
+			// end quotes without space after
 			add_substr_to_cmd(input, command, par->pos, par->i - par->pos, SIMPLE_QUOTES + NO_SPACE_AFTER);
 		else
+			// end quotes with space after
 			add_substr_to_cmd(input, command, par->pos, par->i - par->pos, SIMPLE_QUOTES);
 	}
 	par->pos = par->i + 1;
@@ -46,7 +49,7 @@ void	simple_quotes(char *input, t_list_command **command, t_parse *par)
 
 void	double_quotes(char *input, t_list_command **command, t_parse *par)
 {
-	if(!par->in_double && par->i && !ft_in_charset(input[par->i - 1], WHITESPACES))		
+	if(!par->in_double && par->i && !ft_in_charset(input[par->i - 1], WHITESPACES)) 
 		add_substr_to_cmd(input, command, par->pos, par->i - par->pos - 1, NO_SPACE_AFTER);
 	else if(par->in_double)
 	{
@@ -64,7 +67,7 @@ void	end_word(char *input, t_list_command **command, t_parse *par)
 	// si je suis sur le dernier caractere du mot
 	if(!input[par->i + 1] || ft_in_charset(input[par->i + 1], WHITESPACES))
 	{
-		while(ft_in_charset(input[par->pos], WHITESPACES))
+		while(input[par->pos] && input[par->pos] < 32) //ft_in_charset(input[par->pos], WHITESPACES) but it was causing leaks when only arrows were pressed
 			par->pos++;
 		add_substr_to_cmd(input, command, par->pos, par->i - par->pos + 1, NOTHING);
 		par->pos = par->i + 1;
