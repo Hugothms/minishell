17ec67d923dceadb32590b1712693fe3630796bb
Hugothms <hugothms@gmail.com>
2020-10-19 16:32:10 +0200

    correct memcpy

--- libft/srcs/memory/ft_memcpy.c
+++ libft/srcs/memory/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 16:04:54 by hthomas           #+#    #+#             */
-/*   Updated: 2020/09/30 14:08:40 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/10/19 16:31:19 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
-	if (!dst && !src)
+	if (!dst || !src)
 		return (NULL);
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
-	while (n--)
+	while (n-- > 0)
 		*d++ = *s++;
 	return (dst);
 }
