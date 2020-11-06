b1a905763ec150435713f522812b33cfaf3b0e8c
Dicayne <v.moreau96@hotmail.fr>
2020-11-06 15:02:06 +0100

    echo -nnnnnnn ok, fix segv on export

--- srcs/commands/ft_echo.c
+++ srcs/commands/ft_echo.c
@@ -6,11 +6,29 @@
 /*   By: vmoreau <vmoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 16:16:29 by vmoreau           #+#    #+#             */
-/*   Updated: 2020/10/28 15:20:50 by vmoreau          ###   ########.fr       */
+/*   Updated: 2020/11/04 17:32:25 by vmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int		check_nflag(char *str)
+{
+	int i;
+
+	i = 1;
+	if (!ft_strncmp(str, "-n", 2))
+	{
+		while (str[i] == 'n')
+			i++;
+		if (str[i] != '\0')
+			return (0);
+		else
+			return (1);
+	}
+	else
+		return (0);
+}
 
 char	*ft_echo(t_list_cmd *args)
 {
@@ -20,7 +38,7 @@ char	*ft_echo(t_list_cmd *args)
 	nflag = 0;
 	if (!args || !args->str)
 		return (ft_strdup("\n"));
-	while (!ft_strcmp(args->str, "-n"))
+	while (args && check_nflag(args->str))
 	{
 		nflag = 1;
 		args = args->next;
