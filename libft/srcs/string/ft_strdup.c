43054aa0e5b0ba68e82069e4879a71cf5291a068
Hugothms <hugothms@gmail.com>
2020-11-18 16:53:21 +0100

    strdup clean

--- libft/srcs/string/ft_strdup.c
+++ libft/srcs/string/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vmoreau <vmoreau@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 16:04:54 by hthomas           #+#    #+#             */
-/*   Updated: 2020/11/18 16:29:26 by vmoreau          ###   ########.fr       */
+/*   Updated: 2020/11/18 16:52:51 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ char	*ft_strdup(char const *src)
 	int		len;
 	char	*cpy;
 
-	if (src)
-	{
-		len = ft_strlen(src);
-		if (!(cpy = malloc((len + 1) * sizeof(char))))
-			return (0);
-		ft_memcpy(cpy, src, len);
-		cpy[len] = '\0';
-		return (cpy);
-	}
-	return (NULL);
+	if (!src)
+		return (NULL);
+	len = ft_strlen(src);
+	if (!(cpy = malloc((len + 1) * sizeof(char))))
+		return (0);
+	ft_memcpy(cpy, src, len);
+	cpy[len] = '\0';
+	return (cpy);
 }
