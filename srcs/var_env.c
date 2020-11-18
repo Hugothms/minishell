184d100735f87e53cd0587c133e29b6805837eb3
Hugothms <hugothms@gmail.com>
2020-11-18 16:50:51 +0100

    whitespaces in var ok

--- srcs/var_env.c
+++ srcs/var_env.c
@@ -6,13 +6,13 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 17:46:52 by hthomas           #+#    #+#             */
-/*   Updated: 2020/11/18 14:26:39 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/11/18 16:49:14 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	err_code(t_list_cmd *cmd, t_list *env, int i)
+void		err_code(t_list_cmd *cmd, t_list *env, int i)
 {
 	int		size;
 	int		pos_sep;
@@ -31,20 +31,47 @@ void	err_code(t_list_cmd *cmd, t_list *env, int i)
 	free(after);
 }
 
-void	replace_var_env(t_list_cmd *cmd, t_list *env, int *i, int size)
+static char	*format_var_env(t_list *env, int size)
+{
+	char	*ret;
+	char	*tmp;
+	int		i;
+
+	ret = ft_strtrim(&((char *)env->content)[size], WSP);
+	if (ft_in_charset(((char *)env->content)[size], WSP))
+	{
+		tmp = ft_strjoin(" ", ret);
+		free(ret);
+		ret = tmp;
+	}
+	i = 1;
+	while (ret[i])
+	{
+		if (ft_in_charset(ret[i], WSP) && ft_in_charset(ret[i - 1], WSP))
+			ft_strcpy(&ret[i - 1], &ret[i--]);
+		i++;
+	}
+	ft_printf("str:%s\n", ret);
+	return (ret);
+}
+
+static void	replace_var_env(t_list_cmd *cmd, t_list *env, int *i, int size)
 {
 	char	*after;
+	char	*tmp;
 
 	after = ft_strdup(&cmd->str[*i + size]);
 	cmd->str[*i] = '\0';
-	cmd->str = ft_strjoin_free(cmd->str, &((char *)env->content)[size]);
+	tmp = format_var_env(env ,size);
+	cmd->str = ft_strjoin_free(cmd->str, tmp);
+	free(tmp);
 	size = ft_strlen(cmd->str);
 	cmd->str = ft_strjoin_free(cmd->str, after);
 	*i = size - 1;
 	free(after);
 }
 
-void	test_var_env(t_list_cmd *cmd, t_list *env, int *i)
+void		test_var_env(t_list_cmd *cmd, t_list *env, int *i)
 {
 	int		size;
 
@@ -61,7 +88,7 @@ void	test_var_env(t_list_cmd *cmd, t_list *env, int *i)
 	ft_strcpy(&cmd->str[*i], &cmd->str[*i + size]);
 }
 
-void	replace_all_var_env(t_list_cmd *cmd, t_list *env)
+void		replace_all_var_env(t_list_cmd *cmd, t_list *env)
 {
 	int	i;
 
